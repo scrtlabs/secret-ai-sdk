4a4704d65aa6ec41ea3f7fd75d4a04bc6e86be47
DeDTihoN <tihonmarkp@gmail.com>
2025-08-08 21:21:30 +0300

    updated worker manager contract

--- secret_ai_sdk/_config.py
+++ secret_ai_sdk/_config.py
@@ -22,9 +22,9 @@ LOG_LEVELS = {
 API_KEY = 'SECRET_AI_API_KEY' # api key env var
 
 # SECRET
-SECRET_CHAIN_ID_DEFAULT = 'pulsar-3'
-SECRET_WORKER_SMART_CONTRACT_DEFAULT = 'secret18cy3cgnmkft3ayma4nr37wgtj4faxfnrnngrlq'
-SECRET_NODE_URL_DEFAULT = 'https://pulsar.lcd.secretnodes.com'
+SECRET_CHAIN_ID_DEFAULT = 'secret-4'
+SECRET_WORKER_SMART_CONTRACT_DEFAULT = 'secret1xv90yettghx8uv6ug23knaf5mjqwlsghau6aqa'
+SECRET_NODE_URL_DEFAULT = 'https://lcd.secret.tactus.starshell.net/'
 
 SECRET_CHAIN_ID = 'SECRET_CHAIN_ID' # points to the name of the env var for secret chain id
 SECRET_WORKER_SMART_CONTRACT = 'SECRET_WORKER_SMART_CONTRACT' #points to the env var for the smart contract address for secret worker management
